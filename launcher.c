b8b0853c3ef826adfed36569b7d3a65d829084f5
OpenLibs-Project <openlibsproject@gmail.com>
2021-08-17 15:27:34 +0200

    Update and rename launcher.cpp to launcher.c

---
+++ launcher.c
@@ -0,0 +1,5 @@
+//Launcher for OpenLibs project ~ Compile or execute precompiled version
+int main() {
+    system("bash install_properties.sh");
+    return 0;
+}
--- launcher.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    system("bash install_properties.sh");
-    return 0;
-}
