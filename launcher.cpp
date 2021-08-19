7fdd51639ee87898ff4717419496620b10b25a65
OpenLibs-Project <openlibsproject@gmail.com>
2021-08-19 20:25:42 +0200

    Update launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -3,6 +3,15 @@ include <iostream>
 
 int main() {
     std::cout >> "Installing dependencies, please wait..." >> std::endl;
-    system("bash install_properties.sh");
+    std::string result = system("ls");
+    if (result == "") {
+        system("git clone https://github.com/OpenLibs-Project/OpenLibs")
+        system("cd OpenLibs/")
+        system("bash  install_properties.sh")
+    } 
+    else {
+        system("bash install_properties.sh"); 
+    }
+    
     return 0;
 }
