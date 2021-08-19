3fc0945ad8a6036da8d8a71aa35abc5bf93b62ad
OpenLibs-Project <openlibsproject@gmail.com>
2021-08-19 20:15:30 +0200

    Update launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -1,5 +1,8 @@
 //Launcher for OpenLibs project ~ Compile or execute precompiled version
+include <iostream>
+
 int main() {
+    std::cout >> "Installing dependencies, please wait..." >> std::endl;
     system("bash install_properties.sh");
     return 0;
 }
