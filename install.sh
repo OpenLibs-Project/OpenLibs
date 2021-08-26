e786b74112668b52a5e0cc71ad0c8297019f2005
OpenLibs-Project <openlibsproject@gmail.com>
2021-08-26 20:02:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,22 @@
 
 if ((\`id -u\` != 0))
 then
-  echo 'Please run installer as root or sudo it!'
+  echo 'Please run installer as root or sudo it ~ root privileges needed'
   exit
 fi
 
+cd ..
+cp OpenLibs OpenLibs-v1.0
+rm -rf /usr/local/bin/OpenLibs-v1.0/
+rm -r /usr/local/bin/OpenLibs-v1.0/
+mv OpenLibs-v1.0 /usr/local/bin/
+cd /usr/local/bin/Open:ibs-v1.0/
 
+#python3 charging-screen.py
+#pip3 install pyQt5
 
-
-pip3 install pyQt5
-python3 charging-screen.py
-bash requirements-install.sh
-python3 openlibs.py
+cd ..
+touch openlibs-cli
+echo "python3 /usr/local/bin/OpenLibs-v1.0/openlibs.py" >> openlibs-cli
+chmod +x openlibs-cli
+echo "To run OpenLibs Interactive CLI use <openlibs-cli> command"
