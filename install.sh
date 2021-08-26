e4890892e88bdb7c1a9be175f018dae9ff806e59
OpenLibs-Project <openlibsproject@gmail.com>
2021-08-26 19:56:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Installation script for OpenLibs package manager
+
+if ((\`id -u\` != 0))
+then
+  echo 'Please run installer as root or sudo it!'
+  exit
+fi
+
+
+
+
 pip3 install pyQt5
 python3 charging-screen.py
 bash requirements-install.sh
